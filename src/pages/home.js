6dcb86a923ed9367e11deea1333b55216529255b
izetta <rhobbie09@gmail.com>
2021-02-02 08:34:49 +0700

    hah

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink,
-  Redirect,
-} from 'react-router-dom';
 
-import './home.css';
-// import Navbar from '../parts/navbar';
+import Navbar from '../parts/Navbar';
 import NewEvets from '../parts/newEvents';
 
 function Home() {
   return (
-    <div className="container-home">
-      {/* <Navbar />
-      <h1>Ini adalah halaman Home</h1> */}
-      <NewEvets />
+    <div>
+      <Navbar />
+      <h1>ini Adalah Halaman Home</h1>
     </div>
   );
 }
