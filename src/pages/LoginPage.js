a5b3a3c57675655400652238a144b2bcbe09bfe3
izetta <rhobbie09@gmail.com>
2021-02-04 15:51:01 +0700

    impuving loginPage,RegisterPage Responsive for smartphone and this really cool tetap semangat surip

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import Navbar from '../parts/Navbar';
 import Login from '../components/Login';
+import Aux from 'components/Aux';
 
 export default function LoginPage() {
   return (
     <div>
       <Navbar />
-      <div
-        style={{
-          marginLeft: 40,
-          backgroundColor: '#f5f5f5',
-          marginRight: 31,
-          paddingTop: 20,
-          height: 700,
-        }}
-      >
+      <Aux>
         <Login />
-      </div>
+      </Aux>
     </div>
   );
 }
