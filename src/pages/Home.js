6f9e5c12bee2b2e605d5d2cbe8657932f80a6b31
izetta <rhobbie09@gmail.com>
2021-02-05 14:40:26 +0700

    fixing styling in HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 import Navbar from '../parts/Navbar/index';
 import NewEvents from '../parts/NewEvents/index';
+import Aux from 'components/Aux';
 
 function Home() {
   return (
     <div>
       <Navbar />
       {/* Taruh NewEvents di bungkus div  ini */}
-      <div>
+      <Aux style={{ marginRight: 31, height: 1000 }}>
         <NewEvents />
-      </div>
+      </Aux>
     </div>
   );
 }
