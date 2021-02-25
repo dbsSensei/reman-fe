65d6fb9b71a4d20b82b22ba34e70d8bff9ed0295
Ainun Septian <ainunseptian79@gmail.com>
2021-02-25 23:45:45 +0700

    building details page

--- src/components/DetailsContent/index.js
+++ src/components/DetailsContent/index.js
@@ -7,17 +7,19 @@ const index = () => {
       <div className="banner">
         <h3>Benner Content</h3>
       </div>
-      <div className="sidebar">
-        <div className="color_h2_profile">
-          <div className="setting_h2">
-            <h2 className="the_date">Save The Date</h2>
+      <div className="wrapper_content_details">
+        <div className="sidebar">
+          <div className="color_h2_details_profile">
+            <div className="setting_h2">
+              <h1 className="the_date">Save The Date</h1>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="about">
-        <div className="color_h2_profile">
-          <div className="setting_h2">
-            <h2 className="the_date_2">About The Event</h2>
+        <div className="about">
+          <div className="color_h2_details_profile">
+            <div className="setting_h2">
+              <h1 className="the_date_2">About The Event</h1>
+            </div>
           </div>
         </div>
       </div>
