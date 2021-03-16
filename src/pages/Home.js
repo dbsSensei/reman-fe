4009db4f482392f5433b59e1fea1f1237fe25e44
rhobbikhaidir <rhobbie09@gmail.com>
2021-03-16 14:57:51 +0700

    fixing styling home pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar';
 import Aux from '../components/Aux';
 import Content from '../components/Content';
 import Footer from '../components/Footer';
+import Gap from '../parts/Gap/index';
 
 function Home({ login, setLogin }) {
   console.log(login);
   return (
     <div>
       <Navbar login={login} setLogin={setLogin} />
+      <Gap height={80} />
       {/* Taruh NewEvents di bungkus div  ini */}
       <Aux style={{ marginRight: 31, height: 'auto' }}>
         <Content />
